6f1187b47508c19c3d702667052e1f06b7a4a6e9
han-ish <hanish0019@gmail.com>
2017-10-11 18:03:40 +0530

    Modified client code

--- client.py
+++ client.py
@@ -1,9 +1,30 @@
-from services import TcpDump
+from services import TcpDump, DummyServer
+from servers import Server
+from clients import Client
 import time
 
 tcpdumpObj = TcpDump()
-tcpdumpObj.setinterface("eth0")
+
+# Initializing server and client
+#server = Server()
+server = DummyServer()
+client = Client()
+
+# Setting interface
+tcpdumpObj.setinterface("lo")
+
+# Starting trace
 tcpdumpObj.start_trace()
+
+# Starting dummy server
+server.start()
+
+# Client reading
+time.sleep(1)
+print("client reading here")
+client.read()
 time.sleep(5)
+server.stop()
+print("past that heh")
 tcpdumpObj.stop_trace()
 
